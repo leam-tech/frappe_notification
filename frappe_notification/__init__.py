1fd6d5ad9c7082bcae1f3e3aafdccc8a030f788a
Fahim Ali Zain <fahimalizain@gmail.com>
2022-04-20 09:26:16 +0530

    feat: Notification Channel Fixtures

--- frappe_notification/__init__.py
+++ frappe_notification/__init__.py
@@ -2,3 +2,4 @@
 __version__ = '0.0.1'
 
 from .frappe_notification.doctype.notification_client import NotificationClient, NotificationClientFixtures  # noqa
+from .frappe_notification.doctype.notification_channel import NotificationChannel, NotificationChannelFixtures  # noqa
---
+++ frappe_notification/frappe_notification/doctype/notification_channel/__init__.py
@@ -0,0 +1,2 @@
+from .notification_channel import NotificationChannel  # noqa
+from .test_notification_channel import NotificationChannelFixtures  # noqa
