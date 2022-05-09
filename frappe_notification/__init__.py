8b1f82dbe7d5bfa5a590a334225c0d8cf9f0f933
Fahim Ali Zain <fahimalizain@gmail.com>
2022-05-09 13:50:07 +0530

    tests: aggregate under frappe_notification app

--- frappe_notification/__init__.py
+++ frappe_notification/__init__.py
@@ -7,3 +7,23 @@ from .frappe_notification.doctype.notification_client import NotificationClient,
 from .frappe_notification.doctype.notification_channel import NotificationChannel, NotificationChannelFixtures  # noqa
 from .frappe_notification.doctype.notification_outbox import NotificationOutbox, NotificationOutboxStatus, NotificationOutboxFixtures  # noqa
 from .frappe_notification.doctype.notification_template import NotificationTemplate, NotificationRecipientItem, NotificationTemplateFixtures  # noqa
+
+
+from unittest import TestLoader, TestSuite
+
+
+def load_tests(loader: TestLoader, test_classes, pattern):
+    suite = TestSuite()
+    _test_classes = []
+
+    from .frappe_notification.controllers.tests import get_frappe_notification_controllers_tests
+    from .frappe_notification.doctype.tests import get_frappe_notification_doctype_tests
+
+    _test_classes.extend(get_frappe_notification_doctype_tests())
+    _test_classes.extend(get_frappe_notification_controllers_tests())
+
+    for test_class in _test_classes:
+        t = loader.loadTestsFromTestCase(test_class)
+        suite.addTests(t)
+
+    return suite
