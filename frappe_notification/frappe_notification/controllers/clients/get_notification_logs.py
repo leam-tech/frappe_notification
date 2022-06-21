e999ec5faf59e838ed9590cdaa4ac7ae4f104bc1
Elton Lobo <41537985+e-lobo@users.noreply.github.com>
2022-06-21 08:32:01 +0400

    Cursor pagination get notification logs (#12)
    * feat: cursor pagination get_notification_logs
    
    * test: pass tests
    
    * fix: sory by key error
    
    * refactor: add cursor pagination filters to query
    
    * fix: key error
    
    * fix: order by

--- frappe_notification/frappe_notification/controllers/clients/get_notification_logs.py
+++ frappe_notification/frappe_notification/controllers/clients/get_notification_logs.py
@@ -1,43 +1,97 @@
-from typing import Optional
-
 import frappe
 from frappe_notification import (
     NotificationClientNotFound,
     InvalidRequest,
     get_active_notification_client)
+from typing import Optional
+from frappe_notification.utils.cursor_paginator import CursorPaginator, \
+    CursorPaginatorExecutionArgs, CursorPaginatorSortBy, CursorPaginatorSortByField, \
+    CursorPaginatorSortByDirection
 
 
-def get_notification_logs(
-        channel: Optional[str] = None,
-        channel_id: Optional[str] = None,
-        user_identifier: Optional[str] = None,
-        limit_start: Optional[int] = 0,
-        limit_page_length: Optional[int] = 10,
-        order_by: Optional[str] = "creation desc"):
-    """
-    Fetches a list of Notifications sent to a specific user via specific channel
-    """
+class NotificationLogsFilters(frappe._dict):
+    channel: Optional[str]
+    channel_id: Optional[str]
+    user_identifier: Optional[str]
 
+
+class NotificationLogsSortByEnum(CursorPaginatorSortByField):
+    CREATION = "outbox.creation"
+
+
+class NotificationLogSortBy(CursorPaginatorSortBy):
+    field: NotificationLogsSortByEnum
+
+
+class GetNotificationLogsExecutionArgs(CursorPaginatorExecutionArgs):
+    before: Optional[str]
+    after: Optional[str]
+    first: Optional[int]
+    last: Optional[int]
+    sort_by: Optional[NotificationLogSortBy]
+    filters: NotificationLogsFilters
+
+
+def get_notification_logs(args: GetNotificationLogsExecutionArgs):
+    filters = args.pop('filters', None)
+
+    # TODO: Find a better way to resolve these values..
+    sort_by = args.pop("sort_by", None)
+    if sort_by:
+        sort_by = NotificationLogSortBy({
+            "direction": CursorPaginatorSortByDirection[sort_by.get("direction")],
+            "field": NotificationLogsSortByEnum[sort_by.get("field")]
+        })
+        args["sort_by"] = sort_by
+
+    r = CursorPaginator(
+        doctype="Notification Outbox",
+        skip_process_filters=True,
+        count_resolver=get_notification_logs_count_resolver,
+        node_resolver=get_notification_logs_node_resolver,
+        default_sorting_fields=["outbox.creation"],
+        default_sorting_direction="desc",
+        extra_args={"filters": filters}
+    )
+    return r.execute(args)
+
+
+def get_notification_logs_count_resolver(paginator: CursorPaginator, filters):
+    client = get_active_notification_client()
+    if not client:
+        raise NotificationClientNotFound()
+    notification_filters = paginator.extra_args.get("filters") if paginator.extra_args else None
+    conditions = get_notifications_logs_filters(NotificationLogsFilters(notification_filters or {}))
+
+    return frappe.db.sql(f"""
+    SELECT
+        count(distinct(outbox.name))
+    FROM
+        `tabNotification Outbox` outbox
+    JOIN `tabNotification Outbox Recipient Item` recipient_item
+        ON recipient_item.parent = outbox.name AND recipient_item.parenttype = 'Notification Outbox'
+    WHERE
+        outbox.notification_client = %(client)s
+        AND outbox.docstatus = 1
+        {conditions}
+        AND recipient_item.status = 'Success'
+        """, {
+        "client": client
+    }, as_list=1, debug=0)[0][0]
+
+
+def get_notification_logs_node_resolver(paginator: CursorPaginator, filters, fields, sorting_fields,
+                                        sort_dir, limit):
     client = get_active_notification_client()
     if not client:
         raise NotificationClientNotFound()
 
-    if not ((channel and channel_id) or user_identifier):
-        raise InvalidRequest(message=frappe._(
-            "Please specify either (channel, channel_id) or user_identifier"
-        ))
+    extra_sorting_fields = f", {', '.join(sorting_fields)}"
+    order_by = ', '.join([f'{x} {sort_dir}' for x in sorting_fields])
 
-    conditions = []
-    if channel:
-        conditions.append("recipient_item.channel = %(channel)s")
-
-    if channel_id:
-        conditions.append("recipient_item.channel_id = %(channel_id)s")
-
-    if user_identifier:
-        conditions.append("recipient_item.user_identifier = %(user_identifier)s")
-
-    conditions = " AND {}".format(" AND ".join(conditions)) if len(conditions) else ""
+    notification_filters = paginator.extra_args.get("filters") if paginator.extra_args else None
+    conditions = get_notifications_logs_filters(NotificationLogsFilters(notification_filters or {}),
+                                                filters)
 
     return frappe.db.sql(f"""
     SELECT
@@ -50,23 +104,46 @@ def get_notification_logs(
         recipient_item.channel,
         recipient_item.channel_id,
         recipient_item.seen
+        {extra_sorting_fields}
     FROM
         `tabNotification Outbox` outbox
     JOIN `tabNotification Outbox Recipient Item` recipient_item
-        ON recipient_item.parent = outbox.name AND recipient_item.parenttype = "Notification Outbox"
+        ON recipient_item.parent = outbox.name AND recipient_item.parenttype = 'Notification Outbox'
     WHERE
         outbox.notification_client = %(client)s
         AND outbox.docstatus = 1
         {conditions}
         AND recipient_item.status = "Success"
-    ORDER BY %(order_by)s
-    LIMIT %(limit_start)s, %(limit_page_length)s
+    ORDER BY {order_by}
+    LIMIT %(limit_page_length)s
     """, {
         "client": client,
-        "channel": channel,
-        "channel_id": channel_id,
-        "user_identifier": user_identifier,
-        "limit_start": limit_start,
-        "limit_page_length": limit_page_length,
-        "order_by": order_by
+        "limit_page_length": limit
     }, as_dict=1, debug=0)
+
+
+def get_notifications_logs_filters(filters: NotificationLogsFilters, cursor_filters=None):
+    channel = filters.channel
+    channel_id = filters.channel_id
+    user_identifier = filters.user_identifier
+
+    if not ((channel and channel_id) or user_identifier):
+        raise InvalidRequest(message=frappe._(
+            "Please specify either (channel, channel_id) or user_identifier"
+        ))
+
+    conditions = []
+
+    if cursor_filters:
+        conditions.extend(cursor_filters)
+
+    if channel:
+        conditions.append(f"recipient_item.channel = '{channel}'")
+
+    if channel_id:
+        conditions.append(f"recipient_item.channel_id = '{channel_id}'")
+
+    if user_identifier:
+        conditions.append(f"recipient_item.user_identifier = '{user_identifier}'")
+
+    return f' AND {" AND ".join(conditions)}' if len(conditions) else ""
