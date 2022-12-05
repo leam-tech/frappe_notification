277916a0e987bfec07b9965a147d115733d7877e
Fahim Ali Zain <fahimalizain@gmail.com>
2022-12-05 17:25:03 +0530

    fix: Patch bug

--- frappe_notification/patches/v0/remove_outbox_channel_id_index.py
+++ frappe_notification/patches/v0/remove_outbox_channel_id_index.py
@@ -5,4 +5,11 @@ def execute():
     # Changing FieldType and removing Index on a single migration can issues
     # OperationalError: (1170, "BLOB/TEXT column 'channel_id' used in key specification without a key length")  # noqa
     # Hence we drop the index first. DataType will be updated by frappe migration.
-    frappe.db.sql("ALTER TABLE `tabNotification Outbox Recipient Item` DROP INDEX `channel_id`;")
+    try:
+        frappe.db.sql("ALTER TABLE `tabNotification Outbox Recipient Item` DROP INDEX `channel_id`;")
+    except BaseException:
+        log = frappe.log_error(
+            title="Patch Error: Remove Outbox Channel ID Index",
+            message=frappe.get_traceback()
+        )
+        print("Failed Executing patch. Please check the ErrorLog:", log.name)
